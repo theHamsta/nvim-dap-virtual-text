theHamsta/nvim-dap-virtual-text

commit 4b6508200201538dc5ccc48b7c36378ac7f37a5b

Refactor entrance module to require setup call

lua/nvim-dap-virtual-text.lua
@@ -4,48 +4,28 @@
 --
 -- Distributed under terms of the GPLv3 license.
 --
+local M = {}
 
-local require_ok, dap = pcall(require, "dap")
-if not require_ok then
-  return
-end
+local dap = require 'dap'
 
-local plugin_id = "nvim-dap-virtual-text"
+local plugin_id = 'nvim-dap-virtual-text'
+local options = {
+  enabled = true,
+  all_frames = true,
+}
 
-vim.cmd [[
-  highlight default link NvimDapVirtualText Comment
-  highlight default link NvimDapVirtualTextError LspDiagnosticsVirtualTextError
-  highlight default link NvimDapVirtualTextInfo LspDiagnosticsVirtualTextInfo
-]]
-
-local function on_continue()
-  local virtual_text = require "nvim-dap-virtual-text/virtual_text"
-  virtual_text._on_continue()
-end
-
-dap.listeners.after.event_terminated[plugin_id] = on_continue
-dap.listeners.after.event_exited[plugin_id] = on_continue
-dap.listeners.after.event_continued[plugin_id] = on_continue
-
-dap.listeners.after.event_stopped[plugin_id] = function(_, event)
-  local virtual_text = require "nvim-dap-virtual-text/virtual_text"
-  if event and event.reason == "exception" then
-    virtual_text.set_error("Stopped due to exception")
-  elseif event and event.reason == "data breakpoint" then
-    virtual_text.set_info("Stopped due to " .. event.reason)
-  end
-end
-
--- update virtual text after "variables" request
-dap.listeners.after.variables[plugin_id] = function(session, _, _)
-  local virtual_text = require "nvim-dap-virtual-text/virtual_text"
-  if not vim.g.dap_virtual_text then
-    return
-  end
+local function refresh(session)
+  local virtual_text = require 'nvim-dap-virtual-text/virtual_text'
 
   virtual_text.clear_virtual_text()
 
-  if vim.g.dap_virtual_text == "all frames" and session.threads and session.threads[session.stopped_thread_id] then
+  if not options.enabled then
+    return
+  end
+
+  session = session or dap.session()
+  virtual_text.set_virtual_text(session.current_frame)
+  if all_frames and session.threads and session.threads[session.stopped_thread_id] then
     local frames = session.threads[session.stopped_thread_id].frames
     for _, f in pairs(frames) do
       virtual_text.set_virtual_text(f)
@@ -55,45 +35,106 @@ dap.listeners.after.variables[plugin_id] = function(session, _, _)
   end
 end
 
-dap.listeners.after.stackTrace[plugin_id] = function(session, body, _)
-  local virtual_text = require "nvim-dap-virtual-text/virtual_text"
-  if vim.g.dap_virtual_text and
-    session.stopped_thread_id and session.threads[session.stopped_thread_id] and
-      session.threads[session.stopped_thread_id].frames
-   then
-    local frames_with_source =
-      vim.tbl_filter(
-      function(f)
-        return f.source and f.source.path
-      end,
-      session.threads[session.stopped_thread_id].frames
-    )
-    virtual_text.set_stopped_frame(frames_with_source[1])
+function M.enable()
+  options.enabled = true
+  refresh()
+end
+
+function M.toggle()
+  options.enabled = not options.enabled
+  refresh()
+end
+
+function M.disable()
+  options.enabled = false
+  refresh()
+end
+
+function M.setup(opts)
+  vim.tbl_deep_extend('force', options, opts or {})
+
+  vim.cmd [[
+  highlight default link NvimDapVirtualText Comment
+  highlight default link NvimDapVirtualTextChanged DiagnosticsVirtualTextWarn
+  highlight default link NvimDapVirtualTextError DiagnosticsVirtualTextError
+  highlight default link NvimDapVirtualTextInfo DiagnosticsVirtualTextInfo
+  ]]
+
+  local function on_continue()
+    local virtual_text = require 'nvim-dap-virtual-text/virtual_text'
+    virtual_text._on_continue()
   end
 
-  -- request additional stack frames for "all frames"
-  if vim.g.dap_virtual_text == "all frames" then
-    local requested_functions = {}
+  dap.listeners.after.event_terminated[plugin_id] = on_continue
+  dap.listeners.after.event_exited[plugin_id] = on_continue
+  dap.listeners.after.event_continued[plugin_id] = on_continue
 
-    if body then
-      for _, f in pairs(body.stackFrames) do
-        -- Ensure to evaluate the same function only once to avoid race conditions
-        -- since a function can be evaluated in multiple frames.
-        if not requested_functions[f.name] then
-          if not f.scopes or #f.scopes == 0 then
-            session:_request_scopes(f)
+  dap.listeners.after.event_stopped[plugin_id] = function(_, event)
+    local virtual_text = require 'nvim-dap-virtual-text/virtual_text'
+    if event and event.reason == 'exception' then
+      virtual_text.set_error 'Stopped due to exception'
+    elseif event and event.reason == 'data breakpoint' then
+      virtual_text.set_info('Stopped due to ' .. event.reason)
+    end
+  end
+
+  dap.listeners.after.variables[plugin_id] = function(session, _, _)
+    refresh()
+  end
+
+  dap.listeners.after.stackTrace[plugin_id] = function(session, body, _)
+    if not options.enabled then
+      return
+    end
+
+    local virtual_text = require 'nvim-dap-virtual-text/virtual_text'
+    if
+      session.stopped_thread_id
+      and session.threads[session.stopped_thread_id]
+      and session.threads[session.stopped_thread_id].frames
+    then
+      local frames_with_source = vim.tbl_filter(function(f)
+        return f.source and f.source.path
+      end, session.threads[session.stopped_thread_id].frames)
+      virtual_text.set_stopped_frame(frames_with_source[1])
+    end
+
+    -- request additional stack frames for "all frames"
+    if all_frames then
+      local requested_functions = {}
+
+      if body then
+        for _, f in pairs(body.stackFrames) do
+          -- Ensure to evaluate the same function only once to avoid race conditions
+          -- since a function can be evaluated in multiple frames.
+          if not requested_functions[f.name] then
+            if not f.scopes or #f.scopes == 0 then
+              session:_request_scopes(f)
+            end
+            requested_functions[f.name] = true
           end
-          requested_functions[f.name] = true
         end
       end
     end
   end
+
+  dap.listeners.after.exceptionInfo[plugin_id] = function(_, _, response)
+    local virtual_text = require 'nvim-dap-virtual-text/virtual_text'
+    if not options.enabled then
+      return
+    end
+    virtual_text.set_error(response)
+  end
 end
 
-dap.listeners.after.exceptionInfo[plugin_id] = function(_, _, response)
-  local virtual_text = require "nvim-dap-virtual-text/virtual_text"
-  if not vim.g.dap_virtual_text then
-    return
-  end
-  virtual_text.set_error(response)
+if vim.g.dap_virtual_text then
+  print(
+    '['
+      .. plugin_id
+      .. ']: using g:dap_virtual_text is deprecated!'
+      .. " Please use `require'nvim-dap-virual-text'.setup()'`"
+  )
+  M.setup()
 end
+
+return M

commit f9e6407320940914934f88a429e044b7bca0dae5

Fix typo in new deprecation message
Fixes a small typo for new usage instructions introduced in 4b6508200201538dc5ccc48b7c36378ac7f37a5b

lua/nvim-dap-virtual-text.lua
@@ -157,7 +157,7 @@ if vim.g.dap_virtual_text then
     '['
       .. plugin_id
       .. ']: using g:dap_virtual_text is deprecated!'
-      .. " Please use `lua require'nvim-dap-virual-text'.setup()'`"
+      .. " Please use `lua require'nvim-dap-virtual-text'.setup()'`"
   )
   M.setup()
 end